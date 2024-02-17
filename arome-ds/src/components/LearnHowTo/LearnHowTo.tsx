a61c40066d1f7c94759644908e8e6f909c9ceed2
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-02-17 17:54:27 -0300

    Notebook - 27/02/24 - 17h54 - Programando o LearnHowTo

--- arome-ds/src/components/LearnHowTo/LearnHowTo.tsx
+++ arome-ds/src/components/LearnHowTo/LearnHowTo.tsx
@@ -7,7 +7,7 @@ export const LearnHowTo = () => {
                 <h1 className={classes.text}>Aprenda a montar o seu chá<br/>GOURMET</h1>
             </div>
             
-            <div>
+            <div className={classes.videoConteiner}>
                 <iframe className={classes.video} width="560" height="315" src="https://www.youtube.com/embed/QGp1zGEC93s?si=viEa0LX4UDEJtHjr" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
             </div>
             
