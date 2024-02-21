3f5a0e6879d961c050ffe97f7d4ac48cc7f021fc
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-02-21 12:03:58 -0300

    Notebook - 21/02/24 - 12h03 - Focando na API com JSON

--- arome-ds/src/components/HighlightedProducts/HighlightedProducts.tsx
+++ arome-ds/src/components/HighlightedProducts/HighlightedProducts.tsx
@@ -1,9 +1,18 @@
 import classes from './HighlightedProducts.module.css';
+import { SectionContainer } from '../shared/SectionContainer/SectionContainer'
+
 
 export const HighlightedProducts = () => {
     return(
         <div className={classes.whiteContainer}>
-
+            <SectionContainer>
+                <div className={classes.tittle}>
+                    <h1>Produtos em destaque</h1>
+                </div>
+                <div>
+                    
+                </div>
+            </SectionContainer>
         </div>
     )
 }
