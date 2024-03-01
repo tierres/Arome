a6dd1dfafe236310d7b64dad69199c1faf6735a0
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-03-01 13:49:37 -0300

    notebook 13h49

--- arome-ds/src/components/FindNearestStore/FindNearestStore.tsx
+++ arome-ds/src/components/FindNearestStore/FindNearestStore.tsx
@@ -1,11 +1,17 @@
 import classes from './FindNearestStore.module.css';
 import { SectionContainer } from '../shared/SectionContainer/SectionContainer'
+import storeImg from './arome-store.png'
 
 export const FindNearestStore = () => {
     return(
         <div className={classes.redContainer}>
             <SectionContainer>
-                children
+                <div className={classes.text}>
+
+                </div>
+                <div className={classes.storeImage}>
+                    <img src={storeImg} alt="" />
+                </div>
             </SectionContainer>
         </div>
     )
