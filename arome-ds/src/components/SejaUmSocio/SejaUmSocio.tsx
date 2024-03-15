4633fa5b9501a15ddc2db6efb518056aed6c3227
Tierres Griep <tierresgriep@gmail.com>
2024-03-15 17:33:25 -0300

    PC - 15/03/24 - 17h33 - Trabalhando na socio-img

--- arome-ds/src/components/SejaUmSocio/SejaUmSocio.tsx
+++ arome-ds/src/components/SejaUmSocio/SejaUmSocio.tsx
@@ -1,12 +1,10 @@
 import classes from './SejaUmSocio.module.css'
-import socioImg from './vectors/socio-grop.jpg'
-//import socioImg from './vectors/'
-
+import socioImg from './vectors/work-in-progress-4.jpg'
 
 export const SejaUmSocio = () => {
   return(
     <div className={classes.socioContainer}>
-      <img src={socioImg} alt="" />
+      <img className={classes.img} src={socioImg} alt="" />
     </div>
   );
 };
