0647d9e9d26435fa82c8aff8bc0582826749685b
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-03-19 12:11:54 -0300

    Notebook - 19/03/24 - 12h11

--- arome-ds/src/components/ClubForm/ClubForm.tsx
+++ arome-ds/src/components/ClubForm/ClubForm.tsx
@@ -1,9 +1,25 @@
 import classes from './ClubForm.module.css'
+import { SectionContainer } from '../shared/SectionContainer/SectionContainer';
 
 export const ClubForm = () => {
   return(
     <div className={classes.clubFormContainer}>
-      ClubForm
+      <SectionContainer className={classes.sectionContainer}>
+        <div>
+          <h1 className={classes.tittle}>
+            Faça parte
+          </h1>
+        </div>
+        <div>
+            <p>
+              Nome
+              Email
+              Telefone
+              senha e 
+              endereço
+            </p>
+        </div>
+      </SectionContainer>
     </div>
   );
 };
