28e6f5c8a6ebcb38c6605476c23090bdf06a0836
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-03-20 17:16:04 -0300

    Notebook - 20/03/24 - 17h15 - ClubForm tomando jeito e bordas do Headera arrumadas

--- arome-ds/src/components/ClubForm/ClubForm.tsx
+++ arome-ds/src/components/ClubForm/ClubForm.tsx
@@ -1,7 +1,47 @@
 import classes from './ClubForm.module.css'
 import { SectionContainer } from '../shared/SectionContainer/SectionContainer';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface IForm {
+    name: string
+    email: string
+    phone: string
+    address: string
+    password: string
+}
+
+const initialState: IForm = {
+  name: '',
+  email: '',
+  phone: '',
+  address: '',
+  password: ''
+
+}
 
 export const ClubForm = () => {
+  const [fields, setFields] = useState(initialState)
+
+  const handleFieldsChange = (e: ChangeEvent<HTMLInputElement>) => setFields({
+    ...fields,
+    [e.currentTarget.name]: e.currentTarget.value
+  });
+
+  const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await fetch('https://enf01kcpbh9v.x.pipedream.net/users', {
+      method: 'post',
+      body: JSON.stringify(fields),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setFields(initialState)
+    alert('Cadastro realizado com sucesso.')
+  }
+
+  console.log(fields)
+
   return(
     <div className={classes.clubFormContainer}>
       <SectionContainer className={classes.sectionContainer}>
@@ -10,16 +50,33 @@ export const ClubForm = () => {
             Faça parte
           </h1>
         </div>
-        <div>
-          
+        <div className={classes.form}>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Nome: </label>
+              <input type="text" name='name' value={fields.name} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+              <label htmlFor="email">E-mail: </label>
+              <input type="text" name='email' value={fields.email} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+              <label htmlFor="phone">Telefone: </label>
+              <input type="text" name='phone' value={fields.phone} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+              <label htmlFor="address">Endereço: </label>
+              <input type="text" name='address' value={fields.address} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+              <label htmlFor="password">Senha: </label>
+              <input type="text" name='password' value={fields.password} onChange={handleFieldsChange}/>
+            </div>
+            <br/>
+            <input type='submit'/>
+          </form>
         </div>
       </SectionContainer>
     </div>
   );
 };
-
-              // Nome
-              // Email
-              // Telefone
-              // Endereço 
-              // Senha
