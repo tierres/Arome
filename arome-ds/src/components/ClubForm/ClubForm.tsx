41d8a38dc9d57bbebb023f12772ab816f612d987
Tierres Griep <tierresgriep@gmail.com>
2024-04-05 17:41:37 -0300

    Macbook - 05/04/24 - 17h41 - Arrumar os CSS envolvendo classe dentro de classe e arrumar o tamanho das fontes

--- arome-ds/src/components/ClubForm/ClubForm.tsx
+++ arome-ds/src/components/ClubForm/ClubForm.tsx
@@ -1,67 +1,92 @@
 import classes from './ClubForm.module.css'
 import { SectionContainer } from '../shared/SectionContainer/SectionContainer';
-import { z } from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-const schema = z.object({
-    name: z.string().min(3, 'Por favor, informe um nome válido')
-})
+interface IForm {
+    name: string
+    email: string
+    phone: string
+    address: string
+    password: string
+}
+
+const initialState: IForm = {
+  name: '',
+  email: '',
+  phone: '',
+  address: '',
+  password: ''
+
+}
 
 export const ClubForm = () => {
-    const { handleSubmit, register } = useForm({
-        mode:  'all',
-        criteriaMode: 'all',
-        resolver: zodResolver(schema),
-        defaultValues: {
-            name: ''
-        }
-    });
+  const [fields, setFields] = useState(initialState)
 
-    return(
-        <div className={classes.clubFormContainer}>
-            <SectionContainer className={classes.sectionContainer}>
-                <div>
-                    <h1 className={classes.tittle}>
-                        Faça parte
-                    </h1>
-                </div>
-                <div>
-                    <form className={classes.form}>
-                        <div className={classes.formDiv}>
-                            <label htmlFor="name">Nome: </label>
-                            <br />
-                            <input type="text" name='name' required/>
-                        </div>
-                        <div className={classes.formDiv}>
-                            <label htmlFor="email">E-mail: </label>
-                            <br />
-                            <input type="email" name='email' required/>
-                        </div>
-                        <div className={classes.formDiv}>
-                            <label htmlFor="phone">Telefone: </label>
-                            <br />
-                            <input type="tel" name='phone'/>
-                        </div>
-                        <div className={classes.formDiv}>
-                            <label htmlFor="address">Endereço: </label>
-                            <br />
-                            {/* Configurar para autocompletar o endereço colocando CEP */}
-                            <input type="text" name='address' required/>
-                        </div>
-                        <div className={classes.formDiv}>
-                            <label htmlFor="password">Senha: </label>
-                            <br />
-                            <input type="password" name='password' required/>
-                        </div>
-                        <br />
-                        <br />
-                        <button className={classes.submitButton} type='submit'>
-                            Comece
-                        </button>
-                    </form>
-                </div>
-            </SectionContainer>
+  const handleFieldsChange = (e: ChangeEvent<HTMLInputElement>) => setFields({
+    ...fields,
+    [e.currentTarget.name]: e.currentTarget.value
+  });
+
+  const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await fetch('https://enf01kcpbh9v.x.pipedream.net/users', {
+      method: 'post',
+      body: JSON.stringify(fields),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setFields(initialState)
+    alert('Cadastro realizado com sucesso.')
+  }
+
+  console.log(fields)
+
+  return(
+    <div className={classes.clubFormContainer}>
+      <SectionContainer className={classes.sectionContainer}>
+        <div>
+          <h1 className={classes.tittle}>
+            Faça parte
+          </h1>
         </div>
-    );
+        <div>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <div className={classes.formDiv}>
+              <label htmlFor="name">Nome: </label>
+              <br />
+              <input type="text" name='name' value={fields.name} onChange={handleFieldsChange} required/>
+            </div>
+            <div className={classes.formDiv}>
+              <label htmlFor="email">E-mail: </label>
+              <br />
+              <input type="email" name='email' value={fields.email} onChange={handleFieldsChange} required/>
+            </div>
+            <div className={classes.formDiv}>
+              <label htmlFor="phone">Telefone: </label>
+              <br />
+              <input type="tel" name='phone' value={fields.phone} onChange={handleFieldsChange}/>
+            </div>
+            <div className={classes.formDiv}>
+              <label htmlFor="address">Endereço: </label>
+              <br />
+              {/* Configurar para autocompletar o endereço colocando CEP */}
+              <input type="text" name='address' value={fields.address} onChange={handleFieldsChange} required/>
+            </div>
+            <div className={classes.formDiv}>
+              <label htmlFor="password">Senha: </label>
+              <br />
+              <input type="password" name='password' value={fields.password} onChange={handleFieldsChange} required/>
+            </div>
+            <br />
+            <br />
+            <button className={classes.submitButton} type='submit'>
+              Comece
+            </button>
+            
+          </form>
+        </div>
+      </SectionContainer>
+    </div>
+  );
 };
