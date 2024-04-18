945ecfa760570d4abdfd6a6ae71c04c6c4558f3c
Tierres Griep <tierresgriep@gmail.com>
2024-04-18 19:43:04 -0300

    PC - 18/04/24 - 18h00 - Começando o form novo

--- arome-ds/src/components/ClubForm/original-ClubForm.tsx
+++ arome-ds/src/components/ClubForm/original-ClubForm.tsx
@@ -1,9 +1,46 @@
 import classes from './ClubForm.module.css'
 import { SectionContainer } from '../shared/SectionContainer/SectionContainer';
-import { useForm } from 'react-hook-form'
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface IForm {
+    name: string
+    email: string
+    phone: string
+    address: string
+    password: string
+}
+
+const initialState: IForm = {
+  name: '',
+  email: '',
+  phone: '',
+  address: '',
+  password: ''
+
+}
 
 export const ClubForm = () => {
-    const { register, handleSubmit, errors } = useForm()
+  const [fields, setFields] = useState(initialState)
+
+  const handleFieldsChange = (e: ChangeEvent<HTMLInputElement>) => setFields({
+    ...fields,
+    [e.currentTarget.name]: e.currentTarget.value
+  });
+
+  const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await fetch('https://enf01kcpbh9v.x.pipedream.net/users', {
+      method: 'post',
+      body: JSON.stringify(fields),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setFields(initialState)
+    alert('Cadastro realizado com sucesso.')
+  }
+
+  console.log(fields)
 
   return(
     <div className={classes.clubFormContainer}>
@@ -18,28 +55,28 @@ export const ClubForm = () => {
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="name">Nome: </label>
               <br />
-              <input className={classes.formInput} type="text" name='name' value={fields.name} onChange={handleFieldsChange} ref={register({ required: true })} />
+              <input className={classes.formInput} type="text" name='name' value={fields.name} onChange={handleFieldsChange} required/>
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="email">E-mail: </label>
               <br />
-              <input className={classes.formInput} type="email" name='email' value={fields.email} onChange={handleFieldsChange} ref={register({ required: true })} />
+              <input className={classes.formInput} type="email" name='email' value={fields.email} onChange={handleFieldsChange} required/>
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="phone">Telefone: </label>
               <br />
-              <input className={classes.formInput} type="tel" name='phone' value={fields.phone} onChange={handleFieldsChange} ref={register({ required: true })}/>
+              <input className={classes.formInput} type="tel" name='phone' value={fields.phone} onChange={handleFieldsChange}/>
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="address">Endereço: </label>
               <br />
               {/* Configurar para autocompletar o endereço colocando CEP */}
-              <input className={classes.formInput} type="text" name='address' value={fields.address} onChange={handleFieldsChange} ref={register({ required: true })} />
+              <input className={classes.formInput} type="text" name='address' value={fields.address} onChange={handleFieldsChange} required/>
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="password">Senha: </label>
               <br />
-              <input className={classes.formInput} type="password" name='password' value={fields.password} onChange={handleFieldsChange} ref={register({ required: true })} />
+              <input className={classes.formInput} type="password" name='password' value={fields.password} onChange={handleFieldsChange} required/>
             </div>
             <br />
             <br />
