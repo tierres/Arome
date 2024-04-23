f6be3560f3babb215b80f5a529622b78acc60b3c
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-04-23 11:21:53 -0300

    Notebook - 23/04/24 - 11h21 - Trabalhando no ClubForm

--- arome-ds/src/components/ClubForm/ClubForm.tsx
+++ arome-ds/src/components/ClubForm/ClubForm.tsx
@@ -6,8 +6,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import CircleX from './vectors/circle-x.svg';
 
 const clubMemberSchema = z.object({
-  name: z.string().min(3, "*O nome deve ter pelo menos 3 letras"),
-  email: z.string(),
+  name: z.string().min(3, "*O nome deve ter pelo menos 3 letras")
+    .nonempty("O nome é necessário para o cadastro")
+    .regex(/^[A-Za-z]+$/i, "Only letters are allowed"),
+  email: z.string().email("*O e-mail deve ser válido"),
   phone: z.string(),
   address: z.string(),
   password: z.string()
@@ -37,7 +39,7 @@ export const ClubForm = () => {
         <div>
           <form className={classes.form} onSubmit={handleSubmit(handleClubMember)}>
             <div className={classes.formDiv}>
-              <label className={classes.formLabel} htmlFor="name">Nome: </label>
+              <label className={classes.formLabel} htmlFor="name">*Nome: </label>
               <br />
               <input className={classes.formInput + ' ' + (formState.errors.name ? classes.formInputErrors : "")} {...register('name')} />
               {formState.errors.name && (
@@ -45,24 +47,27 @@ export const ClubForm = () => {
               )}
             </div>
             <div className={classes.formDiv}>
-              <label className={classes.formLabel} htmlFor="email">E-mail: </label>
+              <label className={classes.formLabel} htmlFor="email">*E-mail: </label>
               <br />
-              <input className={classes.formInput} {...register('email')} />
+              <input className={classes.formInput + ' ' + (formState.errors.email ? classes.formInputErrors : "")} {...register('email')} />
+              {formState.errors.email && (
+                <p className={classes.errorsMessage}>{formState.errors.email.message}</p>
+              )}
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="phone">Telefone: </label>
               <br />
-              <input className={classes.formInput} {...register('phone')} />
+              <input className={classes.formInput + ' ' + (formState.errors.phone ? classes.formInputErrors : "")} {...register('phone')} />
             </div>
             <div className={classes.formDiv}>
-              <label className={classes.formLabel} htmlFor="address">Endereço: </label>
+              <label className={classes.formLabel} htmlFor="address">*Endereço: </label>
               <br />
-              <input className={classes.formInput} {...register('address')} />
+              <input className={classes.formInput + ' ' + (formState.errors.address ? classes.formInputErrors : "")} {...register('address')} />
             </div>
             <div className={classes.formDiv}>
-              <label className={classes.formLabel} htmlFor="password">Senha: </label>
+              <label className={classes.formLabel} htmlFor="password">*Senha: </label>
               <br />
-              <input className={classes.formInput} {...register('password')} />
+              <input className={classes.formInput + ' ' + (formState.errors.password ? classes.formInputErrors : "")} {...register('password')} />
             </div>
             <br />
             <br />
