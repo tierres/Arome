3f8735d13578bb05995ce11053a20ae7b59d544f
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-05-02 11:45:14 -0300

    Notebook - 02/05/24 - 11h45 - Working on ClubForm

--- src/components/ClubForm/ClubForm.tsx
+++ src/components/ClubForm/ClubForm.tsx
@@ -11,28 +11,18 @@ const clubMemberSchema = z.object({
   email: z.string().email("*O e-mail deve ser válido"),
   phone: z.string(),
   address: z.string().min(3, "*Um endereço deve ser indicado"),
-  password: z.string().min(8, "*Crie uma senha de no mínimo 8 caracteres, \nsendo: 1 letra maiúscula, 1 número e 1 caractere especial")
+  password: z.string().min(1, "*Crie uma senha de no mínimo 8 caracteres, \nsendo: 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial")
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/, "*A senha deve conter no mínimo 8 caracteres, \nsendo: 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial")
 })
 
 type ClubMembersSchema = z.infer<typeof clubMemberSchema>
 
 interface ICustomDivForm {
   children: string
+  type: string
 }
 
-const CustomDivForm = (params: ICustomDivForm) => {
-  return (
-    <div className={classes.formDiv}>
-      <label className={classes.formLabel} htmlFor="name">*Nome: </label>
-      <br />
-      <input className={classes.formInput + ' ' + (formState.errors.name ? classes.formInputErrors : "")} {...register('name')} />
 
-      {formState.errors.name && (
-      <p className={classes.errorsMessage}>{formState.errors.name.message}</p>
-      )}
-  </div>
-  )
-}
 
 export const ClubForm = () => {
     const { register, handleSubmit , formState } = useForm<ClubMembersSchema>({
@@ -43,6 +33,19 @@ export const ClubForm = () => {
 
     function handleClubMember(data: ClubMembersSchema) {
       console.log(data)
+    }
+
+    const CustomDivForm = (params: ICustomDivForm) => {
+      return (
+        <div className={classes.formDiv}>
+          <label className={classes.formLabel} htmlFor={params.type}>{params.children}</label>
+          <input className={classes.formInput + ' ' + (formState.errors[params.type] ? classes.formInputErrors : "")} {...register(params.type)} />
+    
+          {formState.errors.{params.type} && (
+          <p className={classes.errorsMessage}>{formState.errors.{params.type}.message}</p>
+          )}
+      </div>
+      )
     }
 
   return(
@@ -57,7 +60,6 @@ export const ClubForm = () => {
           <form className={classes.form} onSubmit={handleSubmit(handleClubMember)}>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="name">*Nome: </label>
-              <br />
               <input className={classes.formInput + ' ' + (formState.errors.name ? classes.formInputErrors : "")} {...register('name')} />
               {formState.errors.name && (
                 <p className={classes.errorsMessage}>{formState.errors.name.message}</p>
@@ -65,7 +67,6 @@ export const ClubForm = () => {
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="email">*E-mail: </label>
-              <br />
               <input className={classes.formInput + ' ' + (formState.errors.email ? classes.formInputErrors : "")} {...register('email')} />
               {formState.errors.email && (
                 <p className={classes.errorsMessage}>{formState.errors.email.message}</p>
@@ -73,7 +74,6 @@ export const ClubForm = () => {
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="phone">Telefone: </label>
-              <br />
               <input className={classes.formInput + ' ' + (formState.errors.phone ? classes.formInputErrors : "")} {...register('phone')} />
               {formState.errors.phone && (
                 <p className={classes.errorsMessage}>{formState.errors.phone.message}</p>
@@ -81,7 +81,6 @@ export const ClubForm = () => {
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="address">*Endereço: </label>
-              <br />
               <input className={classes.formInput + ' ' + (formState.errors.address ? classes.formInputErrors : "")} {...register('address')} />
               {formState.errors.address && (
                 <p className={classes.errorsMessage}>{formState.errors.address.message}</p>
@@ -89,7 +88,6 @@ export const ClubForm = () => {
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formLabel} htmlFor="password">*Senha: </label>
-              <br />
               <input className={classes.formInput + ' ' + (formState.errors.password ? classes.formInputErrors : "")} {...register('password')} />
               {formState.errors.password && (
                 <p className={classes.errorsMessage}>{formState.errors.password.message}</p>
