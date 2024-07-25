581caffbc656f843e1b8f30c41d7f7e3944d0f0c
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-07-25 20:00:10 -0300

    Notebook - 25/07/24 - 19h59 - Trabalhando no ProductList

--- frontend/src/components/shared/ProductList/ProductList.tsx
+++ frontend/src/components/shared/ProductList/ProductList.tsx
@@ -7,7 +7,7 @@ export const ProductList = () => {
       <SectionContainer className={classes.sectionContainer}>
         <div className={classes.filtersContainer}>
             <div className={classes.typeFilter}>
-              <label htmlFor="type">Tipo </label>
+              <label htmlFor="type" className={classes.labelStyle}>Tipo </label>
               <select id="type" className={classes.selectStyle}>
                 <option value="">Chá Branco</option>
                 <option value="">Chá Preto</option>
@@ -17,8 +17,8 @@ export const ProductList = () => {
             </div>
             
             <div className={classes.orderFilter}>
-              <label htmlFor="order">Ordenar </label>
-              <select id="order">
+              <label htmlFor="order" className={classes.labelStyle}>Ordenar </label>
+              <select id="order" className={classes.selectStyle}>
                 <option value="">nome A-Z</option>
                 <option value="">nome Z-A</option>
                 <option value="">menores preços</option>
