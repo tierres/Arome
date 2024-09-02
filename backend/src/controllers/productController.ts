76061efaacd7ccd7c1226a0444557fde1b78e388
Tierres Griep <tierresgriep@gmail.com>
2024-09-02 10:38:33 -0300

    PC - 02/09/24 - 10h38

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -6,6 +6,12 @@ export const getProducts = (req: Request, res: Response) => {
     res.json(products)
 }
 
+export const getProduct = (req: Request, res: Response) => {
+    const { id } = req.params
+    const product = db.prepare('SELECT * FROM products WHERE id = ?').get(id)
+    res.json(product)
+}
+
 export const addProduct = (req: Request, res: Response) => {
     const { slug, type, sub_type, name, short_name, price, description, relevance } = req.body
     const stmt = db.prepare('INSERT INTO products (slug, type, sub_type, name, short_name, price, description, relevance) VALUES (?, ?, ?, ?, ?, ?, ?, ?)')
@@ -14,11 +20,6 @@ export const addProduct = (req: Request, res: Response) => {
 }
 
 export const updateProduct = (req: Request, res: Response) => {
-    // 1- Buscar o produto com id
-    // 2- Caso não exista, retorar um erro
-    // 3- Pegar os dados do body
-    // 4- Atualizar (usar UPDATE do sql)
-    // 5- Retornar alguma coisa
     const { id } = req.params
     const product = db.prepare('SELECT * FROM products WHERE id = ?').get(id);
 
@@ -35,5 +36,29 @@ export const updateProduct = (req: Request, res: Response) => {
     res.json({result})
 }
 
-// fazer rota delete(semelhante a rota update)
-// fazer rota getProduct(semelhante a rota update)
+export const deleteProduct = (req: Request, res: Response) => {
+    const { id } = req.params
+    const product = db.prepare('SELECT * FROM products WHERE id = ?').get(id)
+
+    if (!product) {
+        console.log('Produto não encontrado')
+        res.status(404).json({error: 'Produto não encontrado'})
+        return
+    }
+
+    const stmt = db.prepare('DELETE FROM products WHERE id = ?')
+    const result = stmt.run(id)
+
+    if (result.changes > 0) {
+        res.json({ message: 'Produto excluído com sucesso' });
+    } else {
+        res.status(500).json({ error: 'Erro ao excluir o produto' });
+    }
+}
+
+// Para 'updateProduct' e 'deleteProduct': 
+    // 1- Buscar o produto com id
+    // 2- Caso não exista, retorar um erro
+    // 3- Pegar os dados do body
+    // 4- Atualizar (usar UPDATE do sql)
+    // 5- Retornar alguma coisa
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { getProducts, addProduct, updateProduct } from './controllers/productController'
+import { getProducts, getProduct, addProduct, updateProduct, deleteProduct } from './controllers/productController'
 
 const router = Router()
 
 router.get('/products', getProducts)
+router.get('/products/:id', getProduct)
 router.post('/products', addProduct)
 router.put('/products/:id', updateProduct)
+router.delete('/products/:id', deleteProduct)
 
 export default router
