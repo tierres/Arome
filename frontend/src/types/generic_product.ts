9a273bd2ef3841e852f7addd01bc616a8f979b72
Tierres Griep <tierresgriep@gmail.com>
2024-09-04 23:23:01 -0300

    Macbook - 04/09/24 - 23h22 - ProductPage nao carrega o produto

--- frontend/src/types/generic_product.ts
+++ frontend/src/types/generic_product.ts
@@ -1,11 +1,12 @@
 export interface IGenericProduct {
+    id: string
+    slug: string
     type: string
     sub_type: string
-    id: string
     name: string
     short_name: string
     price: number
-    image: string[]
     description: string
     relevance: number
+    image: string[]
   }
