223d59c34a5197645717ca6f9bdfb28fecfe0951
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-09-16 16:43:56 -0300

    Notebook - 16/09/24 - 16h43 - Começando o componente IsntThis

--- frontend/src/components/IsntThis/IsntThis.tsx
+++ frontend/src/components/IsntThis/IsntThis.tsx
@@ -1,12 +1,19 @@
 import classes from './IsntThis.module.css'
 import { SectionContainer } from '../shared/SectionContainer/SectionContainer'
+import IsntThisImg from './vectors/monika-grabkowska-452370-unsplash.svg'
 
 export const IsntThis = () => {
     return(
         <div className={classes.isntThisContainer}>
             <SectionContainer className={classes.sectionContainer}>
-                <div>
-                    Is'nt This ?
+                <div className={classes.paragraphContainer}>
+                    <p>
+                    NÃO ERA ESSE CHÁ QUE BUSCAVA? <br />EXPERIMENTE MONTAR O SEU
+                    </p>
+                </div>
+                <div className={classes.imgAndButtonContainer}>
+                    <img src={IsntThisImg} alt="Aromê" className={classes.imgStyle}/>
+                    <button className={classes.buttonStyle}>Acesse &gt;</button>
                 </div>
             </SectionContainer>
         </div>
