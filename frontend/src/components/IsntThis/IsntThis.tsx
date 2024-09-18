4f02f2e7a9112428eca35f55f55bf7b9bbdc72f5
Tierres NOTEBOOK <tierresgriep@gmail.com>
2024-09-18 12:05:56 -0300

    Notebook - 18/09/24 - 12h05 - O Link em voltar do Button do IsntThis fez o botao descer - Arrumar

--- frontend/src/components/IsntThis/IsntThis.tsx
+++ frontend/src/components/IsntThis/IsntThis.tsx
@@ -3,11 +3,14 @@ import { SectionContainer } from '../shared/SectionContainer/SectionContainer'
 import IsntThisImgTea from './vectors/monika-grabkowska-452370-unsplash.svg'
 import IsntThisImgUtensil from './vectors/conj-cha-utensils.png'
 
+import { Link } from 'react-router-dom'
+
 interface IIsntThisProps {
     productType: string
 }
 
-
+const teasText = <>NÃO ERA ESSE CHÁ QUE BUSCAVA?<br />EXPERIMENTE MONTAR O SEU</>
+const utensilsText = <>NÃO ERA O QUE PROCURAVA?<br />VEJA TODOS NOSSOS UTENSÍLIOS</> 
 
 export const IsntThis = ({ productType }: IIsntThisProps) => {
     return(
@@ -15,12 +18,14 @@ export const IsntThis = ({ productType }: IIsntThisProps) => {
             <SectionContainer className={classes.sectionContainer}>
                 <div className={classes.paragraphContainer}>
                     <p>
-                    NÃO ERA ESSE {productType === 'teas' ? 'CHÁ' : 'PRODUTO'} QUE BUSCAVA? <br />EXPERIMENTE MONTAR O SEU
+                    {productType === 'teas' ? teasText : utensilsText}
                     </p>
                 </div>
                 <div className={classes.imgAndButtonContainer}>
                     <img src={productType === 'teas' ? IsntThisImgTea : IsntThisImgUtensil} alt="Aromê" className={classes.imgStyle}/>
-                    <button className={classes.buttonStyle}>Acesse &gt;</button>
+                    <Link to={productType === 'teas' ? '/build-your' : '/utensils'} className={classes.linkStyle}>
+                        <button className={classes.buttonStyle}>Acesse &gt;</button>
+                    </Link>
                 </div>
             </SectionContainer>
         </div>
