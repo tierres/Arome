f7cb0b524079b4dc2f925340e3357789930f260b
Tierres Griep <tierresgriep@gmail.com>
2024-09-27 14:32:31 -0300

    PC - 27/09/24 - Alteração no README.md

--- frontend/src/components/HighlightedProducts/HighlightedProducts.tsx
+++ frontend/src/components/HighlightedProducts/HighlightedProducts.tsx
@@ -37,6 +37,8 @@ export const HighlightedProducts = () => {
             speed: 500,
             slidesToShow: 4,
             slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
             arrows: true,
             prevArrow: (
                     <img src={LeftArrow} alt="" />
