7635ad9cf2549b8c322ad10e8af04fc68ed8b52f
Tierres Griep <tierresgriep@gmail.com>
2024-10-04 17:47:30 -0300

    PC - 04/10/24 - 17h47 - Alteração do Middleware para servir arquivos estáticos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,8 +24,7 @@ app.get('/', (req, res) => {
 
 // Middleware para servir arquivos estáticos da pasta 'public'
 // Isso permite que você acesse arquivos da pasta public através da URL
-app.use('/public', express.static(path.join(__dirname, 'public'))); 
-// Use a rota '/public' para acessar a pasta 'public'
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Middleware para usar as rotas configuradas no arquivo routes.ts sob o caminho base '/api'
 app.use('/api', routes)
