7fa5da9f697425d0c050b58254d32681b0883a9c
Tierres Griep <tierresgriep@gmail.com>
2024-10-23 22:38:14 -0300

    PC - 23/10/24 - 22h38 - Trabalhando com Media Queries

--- frontend/src/components/shared/Header/Header.tsx
+++ frontend/src/components/shared/Header/Header.tsx
@@ -4,6 +4,7 @@ import Logo from './vectors/logo.svg'
 import Cart from './vectors/cart.svg'
 import Lupa from './vectors/lupa.svg'
 import User from './vectors/user.svg'
+import Menu from './vectors/menu.svg'
 
 import { SectionContainer } from '../SectionContainer/SectionContainer'
 
@@ -60,14 +61,17 @@ export const Header = () => {
         </div>
 
         <div className={classes.iconsLinksContainer}>
-          <button className={classes.buttons}>
+          <button className={classes.cartButton}>
             <img src={Cart} alt="Botão Carrinho" />
           </button>
-          <button className={classes.buttons}>
+          <button className={classes.searchButton}>
             <img src={Lupa} alt="Botão Pesquisar" />
           </button>
-          <button className={classes.buttons}>
+          <button className={classes.userButton}>
             <img src={User} alt="Botão Painel de Usuário" />
+          </button>
+          <button className={classes.menuButton}>
+            <img src={Menu} alt="Botão Menu" />
           </button>
         </div>
       </SectionContainer>
