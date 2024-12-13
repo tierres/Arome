bbdcdf4d32fdd3a2e286e90839482a7d6c4ffae9
Tierres Griep <tierresgriep@gmail.com>
2024-12-13 16:09:17 -0300

    PC - 13/12/24 - 16h08 - Todas as páginas receberam um useEffect que scrolla a página a o top

--- frontend/src/screens/BuildYour/BuildYour.tsx
+++ frontend/src/screens/BuildYour/BuildYour.tsx
@@ -2,7 +2,14 @@ import { Header } from "../../components/shared/Header/Header.tsx";
 
 import { Footer } from "../../components/shared/Footer/Footer.tsx"
 
+import { useEffect } from 'react'
+
 import './BuildYour.css'
+
+// Adiciona o efeito para rolar para o topo
+useEffect(() => {
+  window.scrollTo(0, 0); // Rola a página para o topo
+}, []); // Executa apenas na montagem do componente
 
 export const BuildYour = () => {
   return (
--- frontend/src/screens/Utensils/Utensils.tsx
+++ frontend/src/screens/Utensils/Utensils.tsx
@@ -4,7 +4,14 @@ import { Navigation } from "../../components/shared/Navigation/Navigation.tsx";
 import { ProductList } from "../../components/shared/ProductList/ProductList.tsx";
 import { Footer } from "../../components/shared/Footer/Footer.tsx"
 
+import { useEffect } from 'react'
+
 import './Utensils.css'
+
+// Adiciona o efeito para rolar para o topo
+useEffect(() => {
+  window.scrollTo(0, 0); // Rola a página para o topo
+}, []); // Executa apenas na montagem do componente
 
 export const Utensils = () => {
   return (
